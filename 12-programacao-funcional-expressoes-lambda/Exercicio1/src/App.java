201d1eb38f790f2feb0d3fe000cfa1fd1b91ac5c
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-02-07 13:53:42 -0300

    streams exerciciso lambda

---
+++ 12-programacao-funcional-expressoes-lambda/Exercicio1/src/App.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Product;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter full path to .csv file: ");
+        String path = sc.next();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Product> list = new ArrayList<>();
+            
+            String line = br.readLine();
+            
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+            
+            //Média
+            double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x, y) -> x + y) / list.size();
+            System.out.println("AVERAGE PRICE: " + String.format("%.2f", avg));
+            
+            //Nome dos produtos abaixo da média
+            Comparator<String> comparator = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            List<String> names = list.stream()
+                .filter(p -> p.getPrice() < avg)
+                .map(p -> p.getName())
+                .sorted(comparator.reversed()).collect(Collectors.toList());
+
+            names.forEach(System.out::println);
+
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
---
+++ 12-programacao-funcional-expressoes-lambda/Exercicio2/src/App.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter full path to .csv file: ");
+        String path = sc.next();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Employee> list = new ArrayList<>();
+            
+            String line = br.readLine();
+            
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+            
+            System.out.print("Enter salary: ");
+            double baseSalary = sc.nextDouble();
+
+            //Salario maior que o base
+            List<String> emails = list.stream().filter(e -> e.getSalary() >= baseSalary)
+            .map(x -> x.getEmail()).collect(Collectors.toList());
+            
+            System.out.println("Email of people whose salary is more than " + baseSalary + " :");
+            emails.forEach(System.out::println);
+            
+            
+            //Soma dos salarios de quem comeca com M
+            double sum = list.stream().filter(e -> e.getName().charAt(0) == 'M')
+                .map(e -> e.getSalary()).reduce(0.0, (x, y) -> x + y);
+
+            System.out.println("Sum of salary of people whose name starts with 'M':");
+            System.out.println(sum);
+
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
