f6ec5a5598670ed2686dcb7cdb59a68215df8f3d
pepz <pedro.cruz@gft.com>
2021-04-11 20:32:59 -0300

    projeto api-rest-mongodb

---
+++ 18-projeto-api-rest-mongodb/api-rest-mongo/src/test/java/com/pedro/apirestmongo/ApiRestMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.apirestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
