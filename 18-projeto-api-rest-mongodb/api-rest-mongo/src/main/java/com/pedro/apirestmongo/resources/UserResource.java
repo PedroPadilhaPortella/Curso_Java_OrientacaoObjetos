ce9cd8b22db3ae1e684c3df6c409eefdba23c19e
pepz <pedro.cruz@gft.com>
2021-04-25 00:28:46 -0300

    POST PUT and DELETE

--- 18-projeto-api-rest-mongodb/api-rest-mongo/src/main/java/com/pedro/apirestmongo/resources/UserResource.java
+++ 18-projeto-api-rest-mongodb/api-rest-mongo/src/main/java/com/pedro/apirestmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.pedro.apirestmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pedro.apirestmongo.domain.User;
 import com.pedro.apirestmongo.dto.UserDTO;
@@ -34,5 +37,27 @@ public class UserResource
 		User user = this.service.findById(id);
 		UserDTO userDTO =  new UserDTO(user);
 		return ResponseEntity.ok().body(userDTO);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+		User user = this.service.fromDTO(userDTO);
+		user = this.service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+		User user = this.service.fromDTO(userDTO);
+		user.setId(id);
+		service.update(user);
+		return ResponseEntity.noContent().build();
 	}
 }
--- 18-projeto-api-rest-mongodb/api-rest-mongo/src/main/java/com/pedro/apirestmongo/service/UserService.java
+++ 18-projeto-api-rest-mongodb/api-rest-mongo/src/main/java/com/pedro/apirestmongo/service/UserService.java
@@ -1,3 +1,5 @@
+
+
 package com.pedro.apirestmongo.service;
 
 import java.util.List;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedro.apirestmongo.domain.User;
+import com.pedro.apirestmongo.dto.UserDTO;
 import com.pedro.apirestmongo.repository.UserRepository;
 import com.pedro.apirestmongo.service.exception.ObjectNotFoundException;
 
@@ -23,5 +26,29 @@ public class UserService
 	public User findById(String id) {
 		Optional<User> user = this.repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
+	
+	public void delete(String id) {
+		this.findById(id);
+		repository.deleteById(id);
+	}
+	
+	public User update(User user) {
+		User userDB = findById(user.getId());
+		this.updateData(userDB, user);
+		return repository.save(userDB);
+	}
+	
+	public void updateData(User userDB, User user) {
+		userDB.setName(user.getName());
+		userDB.setEmail(user.getEmail());
 	}
 }
